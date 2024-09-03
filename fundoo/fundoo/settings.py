96758f5f4f92bdcf9170b0e095bedcf50bd1c19d
GANGISETTY RAJ CHARAN <97686664+rajcharanthecoder@users.noreply.github.com>
2024-09-04 00:23:08 +0530

    Update settings.py

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -103,10 +103,10 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysqldb',
-        'USER': 'admin',
-        'PASSWORD': 'admin12345',
-        'HOST': 'mysqldb.ckz7bawd2dzf.ap-south-1.rds.amazonaws.com',
+        'NAME': 'chatapp',
+        'USER': 'root',
+        'PASSWORD': 'Rajcharan2002!',
+        'HOST': '172.31.81.113',
         'PORT': '3306',
     }
 }
