276adac5a83b70d316285124607a82c92d6c426d
GANGISETTY RAJ CHARAN <97686664+rajcharanthecoder@users.noreply.github.com>
2024-09-07 22:50:13 +0530

    Update views.py

--- fundoo/fundooapp/views.py
+++ fundoo/fundooapp/views.py
@@ -7,65 +7,50 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
 from .forms import SignupForm
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from .tokens import account_activation_token
-from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
 
+User = get_user_model()  # Retrieve the user model class from Django.
 
-# this is homepage  
+# Homepage
 def index(request):
     return render(request, 'index.html', {})
 
-
-# login page
+# Login page
 def login_u(request):
     return render(request, 'login.html', {})
 
-
-User = get_user_model()  # will retrieve the USER model class from django.
-
-
 def Signup(request):
     if request.method == 'POST':
-        form = SignupForm(request.POST)  # takes signup data from user
+        form = SignupForm(request.POST)  # Get signup data from user
         if form.is_valid():
             user = form.save(commit=False)
-            user.is_active = True  # user disabled
-            user.save()  # stores in database.
-            # current_site = get_current_site(request)
+            user.is_active = False  # Set user as inactive until email is confirmed
+            user.save()  # Store in the database
+            current_site = get_current_site(request)
             message = render_to_string('acc_email.html', {
                 'user': user,
-                'domain':'http://13.232.251.134/',
-                # 'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),  # because django 2.0.0
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
                 'token': account_activation_token.make_token(user),
             })
-            mail_subject = 'Activate Your ChatApp Account.'  # mail subject
-            to_email = form.cleaned_data.get('email')  # mail id to be sent to
-            email = EmailMessage(mail_subject, message,
-                                 to=[to_email])  # takes 3 arguments mail_subject, message, mail_id to send
-            email.send()  # sends mail
-            return HttpResponse('The confirmation link has been sent to your mail id..\n'
-                                'Please click on given link and confirm your registration....')
-
+            mail_subject = 'Activate Your ChatApp Account'  # Email subject
+            to_email = form.cleaned_data.get('email')  # Recipient email
+            email = EmailMessage(mail_subject, message, to=[to_email])
+            email.send()  # Send email
+            return HttpResponse('The confirmation link has been sent to your email. Please click on the link to confirm your registration.')
     else:
-        # if signup not success
         form = SignupForm()
-
-        # render signup.html
     return render(request, 'signup.html', {'form': form})
-
 
 def activate(request, uidb64, token):
     try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        print('Uid:             :', uid)
-        user = User.objects.get(pk=uid)  # gets the username
-        print('user: ', user)
-    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
@@ -74,28 +59,20 @@ def activate(request, uidb64, token):
     else:
         return HttpResponse('Activation link is invalid!')
 
-import jwt
 def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(username=username, password=password)
-        login(request, user)
         if user:
             if user.is_active:
-                payload = {'username': username,
-                           'password': password, }
-                jwt_token = {'token': jwt.encode(payload, "secret_key", algorithm='HS256')}
-
-                print(jwt_token)
-                return render(request, 'chat.html', {})
-                # return HttpResponse(jwt_token.values())
+                login(request, user)
+                payload = {'username': username}
+                jwt_token = jwt.encode(payload, "secret_key", algorithm='HS256')
+                return render(request, 'chat.html', {'jwt_token': jwt_token.decode('utf-8')})
             else:
-                return HttpResponse("Your account was inactive.")
+                return HttpResponse("Your account is inactive.")
         else:
-            return HttpResponse("Invalid login details given")
-
+            return HttpResponse("Invalid login details.")
     else:
         return render(request, 'login.html', {})
-
-
